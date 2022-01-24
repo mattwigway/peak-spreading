b986569ee681c9c15c61f613a04ec099b1ad66c1
Matt Bhagat-Conway <matt@indicatrix.org>
2022-01-24 14:08:02 -0500

    add fundamental diagrams

--- scripts/fundamental_diagrams.jl
+++ scripts/fundamental_diagrams.jl
@@ -2,15 +2,19 @@
 Create fundamental diagrams using PeMS 5-minute data
 =#
 
-using ArgParse, GLMakie, KFactors, Logging, DataFrames, ProgressMeter, CSV, Colors, Random
+using ArgParse, KFactors, Logging, DataFrames, ProgressMeter, CSV, Colors, Random, RawArray
 
 const CAROLINA_BLUE = colorant"#4B9CD3"
 const RESOLUTION = (1600, 2200)
+const MAX_SPEED_MPH = 100
+const MAX_FLOW_VPH = 3000
+# bumper-to-bumper 20 foot cars
+const MAX_DENSITY_VPM = ceil(Int64, 5280 / 20)
 
 const ARGTABLE = ArgParseSettings()
 @add_arg_table! ARGTABLE begin
     "output_file"
-        help = "File to save fundamental diagrams to"
+        help = "File to save fundamental summary to"
         required = true
         arg_type = String
     "files"
@@ -38,35 +42,37 @@ function main(args)
 
     geo = CSV.read(joinpath(Base.source_dir(), "../data/sensor_meta_geo.csv"), DataFrame)
 
-    res_channel = Channel{Union{DataFrame, Nothing}}()
-
-    # start graph generation thread
-    figtask = Threads.@spawn assemble_graph(res_channel, Progress(length(files)))
+    result = zeros(Int64, (Threads.nthreads(), MAX_SPEED_MPH, MAX_FLOW_VPH, MAX_DENSITY_VPM))
 
     # start reader thread
     tasks = Vector{Task}()
     sizehint!(tasks, length(files))
-    Threads.@spawn begin
-        for file in files
-            # Files read sequentially, avoid disk thrash
+    @showprogress for file in files
+        # Files read sequentially, avoid disk thrash
+        day = nothing
+        for i in 1:3
             day = KFactors.read_day_file(file)
-            task = Threads.@spawn begin
-                proc = process_file($day, geo)
-                put!(res_channel, proc)
-            end
-            push!(tasks, task)
+            isnothing(day) || break
         end
-        wait.(tasks)
-        put!(res_channel, nothing)  # signal that we're done, after all tasks completed
+
+        if isnothing(day)
+            @warn "$file could not be read after 2 retries"
+            continue
+        end
+
+        # NB it is expected that file processing is faster than disk reads. If this is not the
+        # case, memory footprint will expand unbounded, and would need to add something to this loop to prevent
+        # too many files in memory at once.
+        process_file(day, geo, result)
     end
 
-    figure = fetch(figtask)
-    # @info figure.resolution
-    save(args["output_file"], figure, resolution=RESOLUTION)
-    figure
+    # sum up across threads
+    result = dropdims(sum(result; dims=1); dims=1)
+    # compress uses variable width (zigzag) integer encoding, which will help a lot here
+    rawrite(result, args["output_file"]; compress=true)
 end    
 
-function process_file(day, geo)
+function process_file(day, geo, result)
     day = innerjoin(day, geo, on=:station=>:ID)
 
     day.per_lane_flow_vph = day.total_flow ./ day.Lanes .* 12; # 12 5-minute periods per hour, convert to veh/hour
@@ -79,34 +85,18 @@ function process_file(day, geo)
 
     day.vehdens = day.total_flow ./ (day.avg_speed_mph ./ 12) ./ day.Lanes;
 
-    return day
-end
-
-function assemble_graph(channel, p)
-    @info "assembly running"
-    Theme(fontsize=40) |> set_theme!
-
-    fig = Figure(resolution=RESOLUTION)
-
-    density_speed = Axis(fig[1, 1], xlabel="Density (veh/mile)", ylabel="Speed (mph)")
-    speed_flow = Axis(fig[2, 1], xlabel="Speed (mph)", ylabel="Veh/lane/hour")
-    density_flow = Axis(fig[3, 1], xlabel="Density (veh/mile)", ylabel="Veh/lane/hour")
-
-    while true
-        day = take!(channel)
-        isnothing(day) && break
-
-        scatter!(density_speed, day.vehdens, day.avg_speed_mph, markersize=0.01, alpha=0.75, color=CAROLINA_BLUE)
-        scatter!(speed_flow, day.avg_speed_mph, day.per_lane_flow_vph, markersize=0.01, alpha=0.75, color=CAROLINA_BLUE)
-        scatter!(density_flow, day.vehdens, day.per_lane_flow_vph, markersize=0.01, alpha=0.75, color=CAROLINA_BLUE)
-
-        next!(p)
+    for row in Tables.namedtupleiterator(day)
+        process_row(row, result)
     end
-
-    # NB not exactly right, will finish! before progress bar completely done due to empty day files
-    finish!(p)
-
-    fig
 end
 
-FIG = main(ARGS)
+function process_row(row, result)
+    if row.avg_speed_mph ≥ 1 && row.avg_speed_mph < (MAX_SPEED_MPH + 1) &&
+        row.vehdens ≥ 1 && row.vehdens < (MAX_DENSITY_VPM + 1) &&
+        row.per_lane_flow_vph ≥ 1 && row.per_lane_flow_vph < (MAX_FLOW_VPH + 1)
+        result[Threads.threadid(), floor(Int64, row.avg_speed_mph),
+            floor(Int64, row.per_lane_flow_vph), floor(Int64, row.vehdens)] += 1
+    end
+end
+
+main(ARGS)
