e0128568c739dc1475a39a608d34d2ac9e4fc72c
Matthew Wigginton Bhagat-Conway <matt@indicatrix.org>
2022-05-01 15:38:28 -0400

    update computation into separate join and read steps since file sizes are getting so larger

--- src/computation.jl
+++ src/computation.jl
@@ -2,51 +2,7 @@ const DEFAULT_PERMUTATIONS = 1000
 
 const SEED = 867_5309
 
-# Get the relevant period for a particular year - from the third Tuesday of June to 32 days later
-# dates where there was no mask mandate in California
-# statewide mask mandate lifted June 15: https://www.latimes.com/science/story/2021-07-27/timeline-cdc-mask-guidance-during-covid-19-pandemic
-# LA county requires masks again after July 17: https://www.latimes.com/california/story/2021-07-15/l-a-county-will-require-masks-indoors-amid-covid-19-surge
-function period_for_year(year)
-    start = Dates.tonext(d -> Dates.dayofweek(d) == Dates.Tuesday, Date(year, 5, 31)) + Dates.Week(2)
-    endd = start + Dates.Day(32)
-    return start, endd
-end
-
-function period_days_for_year(year)
-    start, endd = period_for_year(year)
-    days = filter(start:Dates.Day(1):endd) do d
-        Dates.dayofweek(d) != Dates.Saturday &&
-        Dates.dayofweek(d) != Dates.Sunday &&
-        !in(d, HOLIDAYS_Δ1)
-    end
-
-    return collect(days)
-end
-
-const PERIODS = Dict(
-    "postpandemic" => [period_for_year(2021)],
-    "pandemic" => [period_for_year(2020)],
-    "prepandemic" => [
-        period_for_year(2019),
-        period_for_year(2018),
-        period_for_year(2017),
-        period_for_year(2016)
-    ]
-)
-
-function period_for_date(date)
-    for (name, dates) in PERIODS
-        for range in dates
-            if date >= range[1] && date <= range[2]
-                return name
-            end
-        end
-    end
-    return missing
-end
-
-# TODO this function is painfully slow. Why? The join? Should we cache the join?
-function read_data(data_path, meta_path; dropmissing=true)
+function join_data(data_path, meta_path)
     sensor_meta = CSV.read(meta_path, DataFrame)
     data = DataFrame(read_parquet(data_path))
 
@@ -57,26 +13,22 @@ function read_data(data_path, meta_path; dropmissing=true)
 
     # reassemble the date and time fields
     data.date = Date.(data.year, data.month, data.day)
-    data.peak_hour_start = passmissing(Time).(data.peak_hour_start_hour, data.peak_hour_start_minute)
+    #data.peak_hour_start = passmissing(Time).(data.peak_hour_start_hour, data.peak_hour_start_minute)
 
     # drop columns no longer needed
 
-    select!(data, Not([:year, :month, :day, :peak_hour_start_hour, :peak_hour_start_minute]))
+    #select!(data, Not([:year, :month, :day, :peak_hour_start_hour, :peak_hour_start_minute]))
 
     # add a period field
-    data.period = CategoricalArray(period_for_date.(data.date))
+    #data.period = CategoricalArray(period_for_date.(data.date))
 
     # how many observations in each period?
-    @info "Observations in each period" combine(groupby(data, :period), nrow)
+    #@info "Observations in each period" combine(groupby(data, :period), nrow)
 
     # add the day of the week
     data.dayofweek = CategoricalArray(Dates.dayname.(data.date))
 
     # add an indicator to indicate a sensor was present in all periods
-    transform!(groupby(data, :station), :period => (v ->
-        any(coalesce.(v .== "pandemic", [false])) &&
-        any(coalesce.(v .== "prepandemic", [false])) &&
-        any(coalesce.(v .== "postpandemic", [false]))) => :present_in_all_periods)
 
     @info "Before filtering, data has $(nrow(data)) rows"
 
@@ -85,33 +37,64 @@ function read_data(data_path, meta_path; dropmissing=true)
     data = data[
         # filter to weekdays
         in.(data.dayofweek, [Set(["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"])]) .&
-        # that are in one of the defined periods
-        .!ismissing.(data.period) .&
         # are not holidays or adjacent
         .!in.(data.date, [HOLIDAYS_Δ1]) .&
-        # and had observations in all periods
-        data.present_in_all_periods .&
-        # and are mainline or conventional highway observations, i.e. not onramps etc
-        in.(data.station_type, [Set(["ML", "CH"])]), :]
+        # and are mainline observations, i.e. not onramps etc
+        (data.station_type .== "ML"),:]
 
 
-    if dropmissing
-        # not missing occupancy data
-        data = data[
-            coalesce.(isfinite.(data.peak_hour_occ), [false]) .&
-            # and had traffic
-            coalesce.(data.total_flow .> 0, [false]), :]
-
-        # and don't have extreme occupancy
-        # this does need to be done in a separate call, because we want it to be based
-        # on the percentiles of the _filtered_ dataset
-        max_occ = percentile(data.peak_hour_occ, 99)
-        @info @sprintf "Removing sensors days with peak-hour occ above 99th percentile (%.2f%%)" max_occ
-        data = data[data.peak_hour_occ .≤ max_occ, :]
-    end
+    # remove columns that cannot be serialized to parquet
+    select!(data, Not([:dayofweek, :date]))
 
     @info "After filtering, data has $(nrow(data)) rows"
     return data
+end
+
+function read_data(data_path; dropmissing = true)
+    data = DataFrame(read_parquet(data_path))
+
+    # not missing occupancy data
+    data.has_missing = .!(
+        coalesce.(isfinite.(data.peak_hour_occ), [false]) .&
+        # and had traffic
+        coalesce.(data.total_flow .> 0, [false])
+    )
+
+    # and don't have extreme occupancy
+    # this does need to be done in a separate call, because we want it to be based
+    # on the percentiles of the _filtered_ dataset
+    max_occ = percentile(data[.!data.has_missing, :peak_hour_occ], 99)
+    @info @sprintf "Removing sensors days with peak-hour occ above 99th percentile (%.2f%%)" max_occ * 100
+    data.has_missing .|= (data.peak_hour_occ .> max_occ)
+
+    if dropmissing
+        data = data[.!data.has_missing, :]
+    end
+
+    # reassemble the date and time fields
+    data.date = Date.(data.year, data.month, data.day)
+    data.peak_hour_start = passmissing(Time).(data.peak_hour_start_hour, data.peak_hour_start_minute)
+
+    # drop columns no longer needed
+
+    select!(data, Not([:year, :month, :day, :peak_hour_start_hour, :peak_hour_start_minute]))
+
+    data
+end
+
+# create data for permutation test
+function create_test_data(data, periods)
+    period = Periods.period_for_date.(data.date, Ref(periods))
+    
+    sensors_in_all_periods = intersect(
+        Set(unique(data[coalesce.(period .== :prepandemic, false), :station])), 
+        Set(unique(data[coalesce.(period .== :lockdown, false), :station])),
+        Set(unique(data[coalesce.(period .== :postlockdown, false), :station]))
+    )
+
+    test_data = data[period .∈ Ref(Set([:prepandemic, :lockdown, :postlockdown])) .&& data.station .∈ Ref(sensors_in_all_periods), :]
+    test_data.period = Periods.period_for_date.(test_data.date, Ref(periods))
+    test_data
 end
 
 function cumulative_dist(v)
@@ -129,10 +112,10 @@ function cumulative_dist(v, w)
 end
 
 const PREPANDEMIC = 1
-const POSTPANDEMIC = 2
+const POSTLOCKDOWN = 2
 
 function permute(data, n_permutations, col)
-    @assert all(in.(data.period, [Set(["prepandemic", "postpandemic"])])) "Permute assumes only pre and post-pandemic data are present"
+    @assert all(in.(data.period, [Set([:prepandemic, :postlockdown])])) "Permute assumes only pre-pandemic and post-lockdown data are present"
 
     output = zeros(Float64, (2, n_permutations))
     dates_and_periods = combine(groupby(data, :date), :period => first => :period)
@@ -143,10 +126,10 @@ function permute(data, n_permutations, col)
     periods = dates_and_periods.period
     
     int_period::Vector{Int64} = map(periods) do period
-        if period == "prepandemic"
+        if period == :prepandemic
             return PREPANDEMIC
-        elseif period == "postpandemic"
-            return POSTPANDEMIC
+        elseif period == :postlockdown
+            return POSTLOCKDOWN
         end
     end
     
@@ -174,9 +157,9 @@ end
 
 function permutation_test(data, col; n_permutations=DEFAULT_PERMUTATIONS)
     means = permute(data, n_permutations, col)
-    sampling_dist_diff_means = means[POSTPANDEMIC, :] .- means[PREPANDEMIC, :]
+    sampling_dist_diff_means = means[POSTLOCKDOWN, :] .- means[PREPANDEMIC, :]
     obs_means = combine(groupby(data, :period), col => mean => :mean)
-    obs_diff = obs_means[obs_means.period .== "postpandemic", :mean][1] - obs_means[obs_means.period .== "prepandemic", :mean][1]
+    obs_diff = obs_means[obs_means.period .== :postlockdown, :mean][1] - obs_means[obs_means.period .== :prepandemic, :mean][1]
     n_sensors = length(unique(data.station))
     
     if obs_diff <= mean(sampling_dist_diff_means)
@@ -187,3 +170,6 @@ function permutation_test(data, col; n_permutations=DEFAULT_PERMUTATIONS)
     
     return (ptest=obs_diff, pval=pval, n_sensors=n_sensors)
 end
+
+idx_for_time(time) = Dates.hour(time) * 12 + Dates.minute(time) ÷ 5 + 1
+time_for_idx(idx) = Dates.Time((idx - 1) ÷ 12, (idx - 1) % 12 * 5)
