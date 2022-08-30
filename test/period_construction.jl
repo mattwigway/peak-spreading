a818e38159ce180d32cca2a9f5ac675424439459
Matt Bhagat-Conway <matt@indicatrix.org>
2022-08-30 11:03:01 -0400

    update period construction test for new period

--- test/period_construction.jl
+++ test/period_construction.jl
@@ -23,7 +23,7 @@ end
     days = KFactors.Periods.period_days_for_year(2022, KFactors.Periods.spring_2022_period)
 
     @test all(days .>= Dates.Date(2022, 2, 16))
-    @test all(days .<= Dates.Date(2022, 4, 18))
+    @test all(days .<= Dates.Date(2022, 8, 18))
     @test in(Dates.Date(2022, 2, 16), days)
     @test in(Dates.Date(2022, 3, 14), days)
     @test in(Dates.Date(2022, 4, 18), days)
@@ -42,11 +42,19 @@ end
     @test KFactors.Periods.period_for_date(Date(2016, 3, 20), KFactors.Periods.SPRING_2022) == :prepandemic
     @test KFactors.Periods.period_for_date(Date(2022, 2, 21), KFactors.Periods.SPRING_2022) |> ismissing # president's day
     @test KFactors.Periods.period_for_date(Date(2022, 2, 22), KFactors.Periods.SPRING_2022) |> ismissing # day after president's day
-    @test KFactors.Periods.period_for_date(Date(2022, 5, 10), KFactors.Periods.SPRING_2022) |> ismissing
+    @test KFactors.Periods.period_for_date(Date(2022, 9, 8), KFactors.Periods.SPRING_2022) |> ismissing
     @test KFactors.Periods.period_for_date(Date(2022, 1, 7), KFactors.Periods.SPRING_2022) |> ismissing
-    @test KFactors.Periods.period_for_date(Date(2019, 5, 18), KFactors.Periods.SPRING_2022) |> ismissing
-    @test KFactors.Periods.period_for_date(Date(2018, 5, 18), KFactors.Periods.SPRING_2022) |> ismissing
-    @test KFactors.Periods.period_for_date(Date(2017, 5, 16), KFactors.Periods.SPRING_2022) |> ismissing
-    @test KFactors.Periods.period_for_date(Date(2016, 5, 16), KFactors.Periods.SPRING_2022) |> ismissing
+    @test KFactors.Periods.period_for_date(Date(2019, 9, 10), KFactors.Periods.SPRING_2022) |> ismissing
+    @test KFactors.Periods.period_for_date(Date(2018, 9, 10), KFactors.Periods.SPRING_2022) |> ismissing
+    @test KFactors.Periods.period_for_date(Date(2017, 9, 12), KFactors.Periods.SPRING_2022) |> ismissing
+    @test KFactors.Periods.period_for_date(Date(2016, 9, 14), KFactors.Periods.SPRING_2022) |> ismissing
     @test KFactors.Periods.period_for_date(Date(2015, 2, 26), KFactors.Periods.SPRING_2022) |> ismissing
+
+    # split lockdown period
+    @test KFactors.Periods.period_for_date(Date(2020, 05, 16), KFactors.Periods.SPRING_2022) == :lockdown
+    @test KFactors.Periods.period_for_date(Date(2021, 03, 16), KFactors.Periods.SPRING_2022) == :lockdown
+    @test KFactors.Periods.period_for_date(Date(2020, 03, 16), KFactors.Periods.SPRING_2022) |> ismissing
+    @test KFactors.Periods.period_for_date(Date(2021, 05, 16), KFactors.Periods.SPRING_2022) |> ismissing
+
+
 end
